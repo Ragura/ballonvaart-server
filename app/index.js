c6307c583e805a875fec2cc42d342d408c9f7071
Steven Schoovaerts <stevenschoovaerts@Stevens-iMac.local>
2019-11-11 20:00:20 +0100

    Model/Controller/Routes voor berichten

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const routerBoekingen = require("./boekingen/router");
+const routerBerichten = require("./berichten/router");
 
 module.exports = (app) => {
   app.use("/boekingen", routerBoekingen);
+  app.use("/berichten", routerBerichten);
 }
